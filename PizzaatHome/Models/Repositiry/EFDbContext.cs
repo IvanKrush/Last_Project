917fc9131d3be489dbb603ffb79529fa7b4766a2
IvanKrush <ivan.krush@oa.edu.ua>
2020-11-20 18:20:08 +0200

    Initial Commit

---
+++ PizzaatHome/Models/Repositiry/EFDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+namespace PizzaatHome.Models.Repositiry
+{
+    public class EFDbContext : DbContext
+    {
+        public DbSet<Pizza> Pizza { get; set; } 
+    }
+}
